597397e9ef72ee0e3a169f1cfc16f5aceed526a5
luizfbalves <luizzbanndera@gmail.com>
2022-10-21 22:01:15 -0300

    fix: nohoist

--- packages/api/src/app.module.ts
+++ packages/api/src/app.module.ts
@@ -3,11 +3,16 @@ import { GraphQLModule } from '@nestjs/graphql';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { ApolloDriver, ApolloDriverConfig } from '@nestjs/apollo';
+import { join } from 'node:path';
 
 @Module({
   imports: [
     GraphQLModule.forRoot<ApolloDriverConfig>({
       driver: ApolloDriver,
+      debug: false,
+      playground: false,
+      autoSchemaFile: join(__dirname, 'src/schema.gql'),
+      sortSchema: true,
     }),
   ],
   controllers: [AppController],
