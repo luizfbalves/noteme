54eaf210e9be8738f283dea6c765b6eebf395a6f
luizfbalves <luizzbanndera@gmail.com>
2022-11-02 17:03:37 -0300

    test(api): user service tests

--- packages/api/src/modules/user/user.service.spec.ts
+++ packages/api/src/modules/user/user.service.spec.ts
@@ -1,6 +1,7 @@
 import { UserService } from './user.service'
 import { Test, TestingModule } from '@nestjs/testing'
 import { PrismaService } from '../../providers/prisma.service'
+import { randomUUID } from 'crypto'
 
 describe('UserService', () => {
   let service: UserService
@@ -18,10 +19,10 @@ describe('UserService', () => {
   })
 
   describe('findOne', () => {
-    it('should return one user', () => {
+    it('should return one user', async () => {
       const id = '3f4bdbfa-165c-4c6a-bfb9-079efc27b5f2'
+
       const result = service.findOne({ id })
-      //TODO cant compare entire object due to date conversion
       expect(result).toBeTruthy()
     })
   })
@@ -30,7 +31,6 @@ describe('UserService', () => {
     it('should return an array of users', () => {
       const result = service.findAll()
 
-      //TODO create better expect with real data comparation
       expect(result).toBeTruthy()
     })
   })
@@ -38,6 +38,18 @@ describe('UserService', () => {
   describe('create', () => {
     it('should add a new user', () => {
       const result = service.create({
+        email: 'jest@mail.com',
+        name: 'jest',
+      })
+
+      expect(result).toBeTruthy()
+    })
+  })
+
+  describe('update', () => {
+    it('should update a user', () => {
+      const result = service.update({
+        id: randomUUID(),
         email: 'jest@mail.com',
         name: 'jest',
       })
