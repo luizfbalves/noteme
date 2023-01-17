0c230a45a574db9b0a4c478d647c9b4bae9c86b2
luizfbalves <luizzbanndera@gmail.com>
2023-01-17 19:20:22 -0300

    .

--- packages/app/src/components/Note/index.tsx
+++ packages/app/src/components/Note/index.tsx
@@ -64,7 +64,10 @@ export const Note: React.FC<TNoteEvent> = (props) => {
       {dialog}
       <Card>
         <div className="card-header">
-          <CloseButton onClick={() => setDialogOpen(true)}>
+          <CloseButton
+            title="delete-button"
+            onClick={() => setDialogOpen(true)}
+          >
             <TbTrashX />
           </CloseButton>
         </div>
