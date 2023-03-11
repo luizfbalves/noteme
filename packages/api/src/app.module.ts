10b3e3c2965bbc2684d2759c6bfefed9c4192fec
luizfbalves <luizzbanndera@gmail.com>
2023-03-11 10:47:37 -0300

    .

--- packages/api/src/app.module.ts
+++ packages/api/src/app.module.ts
@@ -2,17 +2,23 @@ import { NoteModule } from '@modules/note/note.module'
 import { ApolloDriver, ApolloDriverConfig } from '@nestjs/apollo'
 import { Module } from '@nestjs/common'
 import { GraphQLModule } from '@nestjs/graphql'
-import { ApolloServerPluginLandingPageLocalDefault } from 'apollo-server-core'
+import { ApolloServerPluginLandingPageLocalDefault, ApolloServerPluginLandingPageProductionDefault } from 'apollo-server-core'
 import { join } from 'node:path'
 
 @Module({
   imports: [
     GraphQLModule.forRoot<ApolloDriverConfig>({
       driver: ApolloDriver,
+      persistedQueries: false,
+      introspection: false,
       autoSchemaFile: join(process.cwd(), 'src/schema.gql'),
       buildSchemaOptions: { dateScalarMode: 'timestamp' },
       playground: false,
-      plugins: [ApolloServerPluginLandingPageLocalDefault()],
+      path: '/graphql',
+      plugins: [process.env.NODE_ENV ?
+        ApolloServerPluginLandingPageProductionDefault()
+        :
+        ApolloServerPluginLandingPageLocalDefault({ embed: false })],
     }),
     NoteModule,
   ],
--- packages/api/src/main.ts
+++ packages/api/src/main.ts
@@ -4,6 +4,6 @@ import { AppModule } from './app.module'
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule)
-  await app.listen(3333)
+  await app.listen(process.env.PORT || 3333)
 }
 bootstrap()
