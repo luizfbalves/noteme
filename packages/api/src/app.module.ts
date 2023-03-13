1583683674f133acb1dce101afda4445397307d6
luizfbalves <luizzbanndera@gmail.com>
2023-03-13 15:13:43 -0300

    refactor(api): removed dep packages

--- packages/api/src/app.module.ts
+++ packages/api/src/app.module.ts
@@ -2,7 +2,6 @@ import { NoteModule } from '@modules/note/note.module'
 import { ApolloDriver, ApolloDriverConfig } from '@nestjs/apollo'
 import { Module } from '@nestjs/common'
 import { GraphQLModule } from '@nestjs/graphql'
-import { ApolloServerPluginLandingPageDisabled } from 'apollo-server-core'
 import { join } from 'node:path'
 
 @Module({
@@ -15,7 +14,6 @@ import { join } from 'node:path'
       buildSchemaOptions: { dateScalarMode: 'timestamp' },
       playground: false,
       path: '/graphql',
-      plugins: [ApolloServerPluginLandingPageDisabled()],
     }),
     NoteModule,
   ],
