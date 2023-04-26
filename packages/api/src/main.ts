653c1e564add29a6c0adb26127baf564887acaec
luizfbalves <luizzbanndera@gmail.com>
2023-04-25 21:23:26 -0300

    chore: removed many unused deps

--- packages/api/src/main.ts
+++ packages/api/src/main.ts
@@ -4,7 +4,7 @@ import { AppModule } from './app.module'
 
 async function bootstrap() {
 
-  const app = await NestFactory.create(AppModule)
+  const app = await NestFactory.create(AppModule, { cors: true })
   app.listen(3333)
 }
 
--- packages/app/src/services/apollo/apolloClient.ts
+++ packages/app/src/services/apollo/apolloClient.ts
@@ -1,8 +1,16 @@
 import { ApolloClient, HttpLink, InMemoryCache } from "@apollo/client"
 import fetch from 'cross-fetch'
 
+const url = process.env.NODE_ENV === 'production'
+  ?
+  process.env.VITE_API_URL_GRAPHQL
+  :
+  'http://localhost:3333/graphql'
+
+console.log('url', url)
+
 const apolloClient = new ApolloClient({
-  link: new HttpLink({ uri: 'https://noteme-production.up.railway.app/graphql', fetch }),
+  link: new HttpLink({ uri: url, fetch }),
   cache: new InMemoryCache(),
 })
 
