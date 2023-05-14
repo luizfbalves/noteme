084d4a04075e5d338dd9ee16ad0209dea5446752
luizfbalves <luizzbanndera@gmail.com>
2023-05-14 10:48:16 -0300

    .

--- packages/api/src/main.ts
+++ packages/api/src/main.ts
@@ -3,7 +3,6 @@ import { NestFactory } from '@nestjs/core'
 import { AppModule } from './app.module'
 
 async function bootstrap() {
-
   const app = await NestFactory.create(AppModule, { cors: true })
   app.listen(3333)
 }
