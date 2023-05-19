ace9b113d231b6e58a8b4702ba8e77aa5040fa78
luizfbalves <luizzbanndera@gmail.com>
2023-05-18 22:28:20 -0300

    .

--- packages/app/src/services/apollo/apolloClient.ts
+++ packages/app/src/services/apollo/apolloClient.ts
@@ -4,7 +4,7 @@ import { setContext } from '@apollo/client/link/context'
 const url =
   process.env.NODE_ENV === 'production'
     ? process.env.VITE_API_URL_GRAPHQL
-    : 'http://localhost:3333/graphql'
+    : 'http://localhost:3000/dev/graphql'
 
 const httpLink = createHttpLink({
   uri: url,
