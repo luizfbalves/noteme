2c93e9d72c5b45b96443b96f84f0bdbf91214809
luizfbalves <luizzbanndera@gmail.com>
2023-05-20 13:13:44 -0300

    .

--- packages/app/src/services/apollo/apolloClient.ts
+++ packages/app/src/services/apollo/apolloClient.ts
@@ -3,16 +3,17 @@ import { setContext } from '@apollo/client/link/context'
 
 const url =
   process.env.NODE_ENV === 'production'
-    ? process.env.VITE_API_URL_GRAPHQL
-    : 'http://localhost:3333/graphql'
+    ? process.env.VITE_APP_API_GRAPHQL
+    : 'http://localhost:3000/dev/graphql'
 
+console.log(process.env.VITE_APP_API_GRAPHQL)
 const httpLink = createHttpLink({
   uri: url,
 })
 
 const authLink = setContext((_, { headers }) => {
   const { token } = JSON.parse(localStorage.getItem('noteme-user-data'))
-
+  console.log(token)
   return {
     headers: {
       ...headers,
