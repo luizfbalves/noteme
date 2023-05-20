86d2dbc52382b1401db5a67253505b231ea158f0
luizfbalves <luizzbanndera@gmail.com>
2023-05-20 11:42:43 -0300

    fix(api): serverless

--- packages/api/src/app.module.ts
+++ packages/api/src/app.module.ts
@@ -1,7 +1,6 @@
 import { ApolloDriver, ApolloDriverConfig } from '@nestjs/apollo'
 import { Module } from '@nestjs/common'
 import { GraphQLModule } from '@nestjs/graphql'
-import { join } from 'node:path'
 
 import { AuthModule } from './modules/auth/auth.module'
 import { NoteModule } from './modules/note/note.module'
@@ -10,7 +9,7 @@ import { NoteModule } from './modules/note/note.module'
   imports: [
     GraphQLModule.forRoot<ApolloDriverConfig>({
       driver: ApolloDriver,
-      autoSchemaFile: join(process.cwd(), 'src/schema.gql'),
+      autoSchemaFile: true,
       buildSchemaOptions: { dateScalarMode: 'timestamp' },
       playground: process.env.NODE_ENV === 'development' ? true : false,
       introspection: process.env.NODE_ENV === 'development' ? true : false,
